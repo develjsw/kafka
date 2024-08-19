11a57df9b0c30b9bf26d8c962f01ab2986fdbb7a
jsw <develjsw@gmail.com>
2024-08-19 19:48:49 +0900

    local 환경 broker host 수정

--- src/config/local.config.ts
+++ src/config/local.config.ts
@@ -3,9 +3,9 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('config', () => ({
     kafka: {
         brokers: {
-            address1: 'localhost:9095',
-            address2: 'localhost:9096',
-            address3: 'localhost:9097',
+            address1: 'kafka-1:9095',
+            address2: 'kafka-2:9096',
+            address3: 'kafka-3:9097',
         }
     }
 }))
